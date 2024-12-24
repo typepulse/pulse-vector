1262cca434e0665996323220fa609ca0cc48863d
taishikato <taishi.k0903@gmail.com>
2024-12-24 23:31:42 +0900

    Remove RSS feed link from layout to streamline code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,12 +22,6 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://api.fontshare.com/css?f%5B%5D=switzer@400,500,600,700&amp;display=swap"
         />
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="The Radiant Blog"
-          href="/blog/feed.xml"
-        />
       </head>
       <body className="text-gray-950 antialiased">{children}</body>
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS!} />
