c536f4d9d78898d4cf056a51a71046b1a8b9462a
taishikato <taishi.k0903@gmail.com>
2025-01-02 11:00:48 +0900

    Rename middleware file

--- apps/web/src/utils/supabase/middleware.ts
+++ apps/web/src/utils/supabase/middleware.ts
@@ -41,9 +41,7 @@ export async function updateSession(request: NextRequest) {
   } = await supabase.auth.getUser();
 
   if (
-    !user &&
-    !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
+    !user && request.nextUrl.pathname.startsWith("/dashboard")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
