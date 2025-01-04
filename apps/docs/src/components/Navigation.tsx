4754b1daaa03eef83779c36471469f3e763cdfb2
taishikato <taishi.k0903@gmail.com>
2025-01-04 13:18:04 +0900

    remove commented-out sections from Navigation component

--- apps/docs/src/components/Navigation.tsx
+++ apps/docs/src/components/Navigation.tsx
@@ -242,28 +242,6 @@ export const navigation: Array<NavGroup> = [
     title: 'Retrieval',
     links: [{ title: 'Search', href: '/endpoint/embeddings/embeddings' }],
   },
-  // {
-  //   title: 'Guides',
-  //   links: [
-  //     { title: 'Introduction', href: '/' },
-  //     { title: 'Quickstart', href: '/quickstart' },
-  //     { title: 'SDKs', href: '/sdks' },
-  //     { title: 'Authentication', href: '/authentication' },
-  //     { title: 'Pagination', href: '/pagination' },
-  //     { title: 'Errors', href: '/errors' },
-  //     { title: 'Webhooks', href: '/webhooks' },
-  //   ],
-  // },
-  // {
-  //   title: 'Resources',
-  //   links: [
-  //     { title: 'Contacts', href: '/contacts' },
-  //     { title: 'Conversations', href: '/conversations' },
-  //     { title: 'Messages', href: '/messages' },
-  //     { title: 'Groups', href: '/groups' },
-  //     { title: 'Attachments', href: '/attachments' },
-  //   ],
-  // },
 ]
 
 export function Navigation(props: React.ComponentPropsWithoutRef<'nav'>) {
