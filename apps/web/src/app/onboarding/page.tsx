b8c86481450da0861cbdae2df8f9c41b99ffbf67
taishikato <taishi.k0903@gmail.com>
2025-01-13 12:12:31 +0900

    Add logo and app name to onboarding layout

--- apps/web/src/app/onboarding/page.tsx
+++ apps/web/src/app/onboarding/page.tsx
@@ -1,15 +1,5 @@
 import { OnboardingForm } from "@/components/onboarding-form";
-import { APP_NAME } from "../consts";
 
 export default function OnboardingPage() {
-  return (
-    <>
-      <div className="flex items-center justify-center gap-2">
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/logo.png" alt="Supavec Logo" className="size-10" />
-        <h1 className="text-2xl font-bold">{APP_NAME}</h1>
-      </div>
-      <OnboardingForm />
-    </>
-  );
+  return <OnboardingForm />;
 }
