bacd2a072c10931b82f94b8bcefdbbb9e2bf7de7
taishikato <taishi.k0903@gmail.com>
2025-01-18 20:07:50 +0900

    Remove environment variable checks for PostHog client

--- packages/api/src/utils/posthog.ts
+++ packages/api/src/utils/posthog.ts
@@ -1,14 +1,6 @@
 import { PostHog } from "posthog-node";
 
-if (!process.env.POSTHOG_API_KEY) {
-  throw new Error("POSTHOG_API_KEY is required");
-}
-
-if (!process.env.POSTHOG_HOST) {
-  throw new Error("POSTHOG_HOST is required");
-}
-
 // Initialize PostHog client
-export const client = new PostHog(process.env.POSTHOG_API_KEY, {
-  host: process.env.POSTHOG_HOST,
+export const client = new PostHog(process.env.POSTHOG_API_KEY!, {
+  host: process.env.POSTHOG_HOST!,
 });
