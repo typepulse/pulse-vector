b6ee3ddbafacad2c5d99b03efd6a2d2b1252dd1b
taishikato <taishi.k0903@gmail.com>
2025-01-20 12:09:46 +0900

    Replace button with div in feature selector component

--- apps/web/src/components/feature-selector.tsx
+++ apps/web/src/components/feature-selector.tsx
@@ -20,15 +20,15 @@ export const FeatureSelector: React.FC<FeatureSelectorProps> = ({
       <div className="md:col-span-2 border-b md:border-b-0 bg-background md:border-r border-border sticky top-[var(--header-height)]">
         <div className="flex md:flex-col md:h-full feature-btn-container overflow-x-auto p-4 pb-2">
           {features.map((option) => (
-            <button
+            <div
               key={option.id}
-              className={`flex-shrink-0 md:flex-1 w-64 md:w-full text-left p-4 mb-2 mr-2 last:mr-0 md:mr-0 rounded border border-border`}
+              className="flex-shrink-0 md:flex-1 flex flex-col justify-center w-64 md:w-full text-left p-4 mb-2 mr-2 last:mr-0 md:mr-0 rounded border border-border"
             >
               <h3 className="font-medium tracking-tight">{option.title}</h3>
               <p className="text-sm text-muted-foreground">
                 {option.description}
               </p>
-            </button>
+            </div>
           ))}
         </div>
       </div>
