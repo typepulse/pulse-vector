3cb2f4ca533c84be228fe7bc18a956118827ea98
taishikato <taishi.k0903@gmail.com>
2025-01-29 16:47:00 +0700

    Add basic query submission functionality to Chat with PDF demo

---
+++ apps/web/src/app/api/demo/chat-with-pdf/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import axios from "redaxios";
+
+export async function POST(req: NextRequest) {
+  const reqJson = await req.json();
+
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/embeddings`,
+      {
+        query: reqJson.query,
+        file_ids: ["fcf9d747-e22d-4803-9678-69f80bc15b32"],
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          authorization: process.env.DEMO_SUPA_API_KEY!,
+        },
+      },
+    );
+
+    console.log({ response });
+    return NextResponse.json({ status: "done" });
+  } catch (error) {
+    if (
+      error && typeof error === "object" && "data" in error && error.data &&
+      typeof error.data === "object" && "error" in error.data
+    ) {
+      console.log({ error: error.data.error });
+      return NextResponse.json({
+        success: false,
+        error: error.data.error,
+      }, { status: 400 });
+    }
+    return NextResponse.json({
+      success: false,
+      error: "An unexpected error occurred",
+    }, { status: 400 });
+  }
+}
--- apps/web/src/app/examples/chat-with-pdf/_components/demo.tsx
+++ apps/web/src/app/examples/chat-with-pdf/_components/demo.tsx
@@ -3,11 +3,20 @@
 import { FileUploadForm } from "@/app/dashboard/file-upload-form";
 
 export const Demo = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    fetch("http://localhost:3000/api/demo/chat-with-pdf", {
+      method: "POST",
+      body: JSON.stringify({ query: "hello" }),
+    });
+    e.preventDefault();
+    console.log("submitted");
+  };
+
   return (
     <div className="space-y-8 min-w-[400px] w-full border rounded-3xl p-4 mt-8">
       <FileUploadForm apiKey={`fdshj`} />
       <div className="relative max-w-xl w-full mx-auto flex flex-col gap-2">
-        <div className="relative">
+        <form className="relative" onSubmit={handleSubmit}>
           <textarea
             className="mx-auto h-[52px] flex border border-input px-3 shadow-sm focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm max-w-xl bg-black/5 dark:bg-white/5 w-full rounded-2xl sm:rounded-3xl pl-10 sm:pl-12 pr-12 sm:pr-16 placeholder:text-black/70 dark:placeholder:text-white/70 border-none ring-black/30 dark:ring-white/30 text-black dark:text-white text-wrap py-3 sm:py-4 text-sm sm:text-base max-h-[200px] overflow-y-auto resize-none leading-[1.2] min-h-[52px]"
             id="ai-input-with-file"
@@ -16,7 +25,7 @@ export const Demo = () => {
           />
           <button
             className="absolute right-2 sm:right-3 top-1/2 -translate-y-1/2 rounded-xl bg-black/5 dark:bg-white/5 py-1 px-1"
-            type="button"
+            type="submit"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -34,7 +43,7 @@ export const Demo = () => {
               <path d="M4 20h7a4 4 0 0 0 4-4V4"></path>
             </svg>
           </button>
-        </div>
+        </form>
       </div>
     </div>
   );
